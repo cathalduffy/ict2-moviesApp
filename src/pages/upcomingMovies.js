67da6b4a7715a074a1fe40249478c60d23d6aaac
Cathal Duffy <cathalduffy@cathals-mbp.home>
2022-07-16 20:51:04 +0100

    Fix upcoming movies page.

--- src/pages/upcomingMovies.js
+++ src/pages/upcomingMovies.js
@@ -1,32 +1,36 @@
-import React, { useState, useEffect } from "react";
-import PageTemplate from '../components/templateMovieListPage';
-import { getUpcomingMovies } from "../api/tmdb-api";
+import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { useQuery } from 'react-query'
+import Spinner from '../components/spinner'
+import {getUpcomingMovies} from '../api/tmdb-api'
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+
 
 const UpcomingPage = (props) => {
-  const [movies, setMovies] = useState([]);
-  //const favourites = movies.filter(m => m.favourite)
-  //localStorage.setItem('favourites', JSON.stringify(favourites))
+  const {  data, error, isLoading, isError }  = useQuery('discover', getUpcomingMovies)
 
-/*  const addToFavourites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favourite: true } : m
-    );
-    setMovies(updatedMovies);
-  };*/
+  if (isLoading) {
+    return <Spinner />
+  }
 
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // These three lines are redundant; we will replace them laterg.
+  const favourites = movies.filter(m => m.favouurite)
+  localStorage.setItem('favourites', JSON.stringify(favourites))
 
   return (
     <PageTemplate
-      title='Upcoming Movies'
+      title="Discover Movies"
       movies={movies}
-     // selectFavourite={addToFavourites}
+      action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
     />
-  );
+);
 };
+
 export default UpcomingPage;
